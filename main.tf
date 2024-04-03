a17aa972017742a235b99c9bbfa274c88f6ca86b
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-03 22:34:50 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "rg" {
+  source  = "app.terraform.io/ddi-support/rg/azurerm"
+  version = "1.0.0"
+  rgname = var.rgname
+  rglocation = var.rglocation
+  planname = var.planname
+  kind = var.kind
+  # insert required variables here
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "rgname" {
+    type = string
+  
+}
+variable "rglocation" {
+    type = string
+  
+}
+variable "planname" {
+  type = string
+
+}
+
+variable "kind" {
+    type = string
+  
+}
