ae2fb206345befda17ba80460f4177c00e57d49d
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-04 08:32:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
+  }
+}
 
+provider "azurerm" {
+  # Configuration options
+}
