34be4a21b23b9fa30d44806d9153a9cd84412e1c
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-04 08:06:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.97.1"
-    }
-  }
-}
 
-provider "azurerm" {
-    features {
-      
-    }
-  #client_id       = "fdc290e0-4b4b-47d8-957e-3529e56d999e"
-  #client_secret   = "LIS8Q~00IzJNiPzv.yLZK7tFNlJRKTdIeqzbEa6-"
-  #subscription_id = "f6693f88-cd18-4473-863f-de38c28715df"
-  #tenant_id       = "7cc14b5b-fabc-48c4-82ac-570f0099fec2"
-}
