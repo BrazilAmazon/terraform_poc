f19c083cc3734b5dc2ffc96fc4c3d3243540a683
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-04 08:06:52 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,10 @@
 terraform {
   cloud {}
-  required_providers {
-        ##...
+ required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
   }
   required_version = ">= 1.2.0"
 }
