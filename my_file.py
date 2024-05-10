92aea46a68af1856fe052cf6bf1713a4eb306374
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-05-10 20:21:59 +0530

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -1,6 +1,6 @@
 import sys, os
 
-print(f'::set-output name=Test::"Ab"')
+print(f'::set-output name=Test::{"Ab"}')
 first_name = sys.argv[1]
 last_name = sys.argv[2]
 print("Hello " + first_name + " " + last_name)
