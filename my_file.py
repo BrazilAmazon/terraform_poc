f92d0a40334d8ef7aa1f192983ef70444848af7e
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-05-10 20:24:58 +0530

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -1,6 +1,7 @@
 import sys, os
 
-print(f'::set-output name=Test::{"Ab"}')
+cmd='echo "TEST=AB" >> $GITHUB_OUTPUT'
+subprocess.call([str(cmd)], shell=False)
 first_name = sys.argv[1]
 last_name = sys.argv[2]
 print("Hello " + first_name + " " + last_name)
