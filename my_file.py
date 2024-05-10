cc15dfe8ef90e990ee86e05f53209372fcf548c3
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-05-11 00:53:36 +0530

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -1,4 +1,6 @@
-import time
-while 1:
-  print("hey",flush=True)
-  time.sleep(5)
+
+import os
+name = 'my_name'
+value = 'my_value'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={value}', file=fh)
