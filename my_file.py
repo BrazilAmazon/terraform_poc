c32084875a6ea7402cf90b66b93ec72b580f87d8
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-05-10 20:29:33 +0530

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -1,7 +1,4 @@
-import sys, os, subprocess
-
-cmd='echo "TEST=AB" >> $GITHUB_OUTPUT'
-subprocess.call([str(cmd)], shell=False)
+import sys
 first_name = sys.argv[1]
 last_name = sys.argv[2]
 print("Hello " + first_name + " " + last_name)
